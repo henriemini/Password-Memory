5a810d2ca2f1543f844a691611a6247e6f8b990a
Henrique Iemini <19henrique97@gmail.com>
2025-06-03 22:43:00 -0300

    feat: implementation findAll on userService

--- src/app/services/user/user-read.service.ts
+++ src/app/services/user/user-read.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { firstValueFrom } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserReadService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  findAll() : Promise<any> {
+
+    // para consumir este metodo basta utilizar a palavra chave: await
+    // let X = await userReadService.finalAll();
+    return firstValueFrom(this.http.get<any>(`${environment.api_endpoint}/user`));0
+    }
+
 }
