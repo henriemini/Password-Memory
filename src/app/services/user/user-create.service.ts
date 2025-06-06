45d74e5409d859f2c41069a8a441f11829895dc7
Henrique Iemini <19henrique97@gmail.com>
2025-06-06 20:09:57 -0300

    feat: add user create service

--- src/app/services/user/user-create.service.ts
+++ src/app/services/user/user-create.service.ts
@@ -1,9 +1,16 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { User } from '../../domain/model/user';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserCreateService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  create(user: User){
+    return this.http.post(`${environment.api_endpoint}/user`,user);
+  }
 }
